6b450c19773e0205996b0c21af9a82f2c2120873
TANABE Ken-ichi <nabeken@tknetworks.org>
2014-12-21 19:58:16 +0900

    initial import

---
+++ sqs/option/option.go
@@ -0,0 +1,77 @@
+package option
+
+import (
+	"strconv"
+
+	"github.com/stripe/aws-go/aws"
+	"github.com/stripe/aws-go/gen/sqs"
+)
+
+const (
+	DataTypeString = "String"
+	DataTypeNumber = "Number"
+	DataTypeBinary = "Binary"
+)
+
+type ReceiveMessageRequest func(req *sqs.ReceiveMessageRequest)
+
+func VisibilityTimeout(timeout int) ReceiveMessageRequest {
+	return func(req *sqs.ReceiveMessageRequest) {
+		req.VisibilityTimeout = aws.Integer(timeout)
+	}
+}
+
+func MaxNumberOfMessages(n int) ReceiveMessageRequest {
+	return func(req *sqs.ReceiveMessageRequest) {
+		req.MaxNumberOfMessages = aws.Integer(n)
+	}
+}
+
+type SendMessageRequest func(req *sqs.SendMessageRequest)
+
+func DelaySeconds(delay int) SendMessageRequest {
+	return func(req *sqs.SendMessageRequest) {
+		req.DelaySeconds = aws.Integer(delay)
+	}
+}
+
+func MessageAttributes(attrs map[string]interface{}) SendMessageRequest {
+	return func(req *sqs.SendMessageRequest) {
+		if len(attrs) == 0 {
+			return
+		}
+
+		ret := make(map[string]sqs.MessageAttributeValue)
+		for n, v := range attrs {
+			ret[n] = MessageAttributeValue(v)
+		}
+		req.MessageAttributes = ret
+	}
+}
+
+func MessageAttributeValue(v interface{}) sqs.MessageAttributeValue {
+	switch vv := v.(type) {
+	case string:
+		return sqs.MessageAttributeValue{
+			DataType:    aws.String(DataTypeString),
+			StringValue: aws.String(vv),
+		}
+	case []byte:
+		return sqs.MessageAttributeValue{
+			DataType:    aws.String(DataTypeBinary),
+			BinaryValue: vv,
+		}
+	case int64:
+		return sqs.MessageAttributeValue{
+			DataType:    aws.String(DataTypeNumber),
+			StringValue: aws.String(strconv.FormatInt(vv, 10)),
+		}
+	case int:
+		return sqs.MessageAttributeValue{
+			DataType:    aws.String(DataTypeNumber),
+			StringValue: aws.String(strconv.FormatInt(int64(vv), 10)),
+		}
+	default:
+		panic("sqs: unsupported type")
+	}
+}
---
+++ sqs/queue.go
@@ -0,0 +1,74 @@
+package sqs
+
+import (
+	"github.com/nabeken/aws-go-sqs/sqs/option"
+
+	"github.com/stripe/aws-go/aws"
+	"github.com/stripe/aws-go/gen/sqs"
+)
+
+type Queue struct {
+	*sqs.SQS
+	url aws.StringValue
+}
+
+func New(s *sqs.SQS, name string) (*Queue, error) {
+	u, err := GetQueueURL(s, name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Queue{
+		SQS: s,
+		url: u,
+	}, nil
+}
+
+func (q *Queue) SendMessage(body string, opts ...option.SendMessageRequest) error {
+	req := &sqs.SendMessageRequest{
+		MessageBody: aws.String(body),
+		QueueURL:    q.url,
+	}
+
+	for _, f := range opts {
+		f(req)
+	}
+
+	_, err := q.SQS.SendMessage(req)
+	return err
+}
+
+func (q *Queue) ReceiveMessage(opts ...option.ReceiveMessageRequest) ([]sqs.Message, error) {
+	req := &sqs.ReceiveMessageRequest{
+		QueueURL: q.url,
+	}
+
+	for _, f := range opts {
+		f(req)
+	}
+
+	resp, err := q.SQS.ReceiveMessage(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Messages, nil
+}
+
+func (q *Queue) DeleteMessage(receiptHandle aws.StringValue) error {
+	return q.SQS.DeleteMessage(&sqs.DeleteMessageRequest{
+		QueueURL:      q.url,
+		ReceiptHandle: receiptHandle,
+	})
+}
+
+func GetQueueURL(s *sqs.SQS, name string) (aws.StringValue, error) {
+	req := &sqs.GetQueueURLRequest{
+		QueueName: aws.String(name),
+	}
+
+	resp, err := s.GetQueueURL(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.QueueURL, nil
+}
