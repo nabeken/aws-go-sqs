70c78ab3fdf60c81cc8a8c857f206459875434e5
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-01-10 22:29:51 +0900

    queue: Add SendMessageBatch()

--- queue/queue.go
+++ queue/queue.go
@@ -1,10 +1,13 @@
 package queue
 
 import (
-	"github.com/nabeken/aws-go-sqs/queue/option"
+	"fmt"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/stripe/aws-go/aws"
 	"github.com/stripe/aws-go/gen/sqs"
+
+	"github.com/nabeken/aws-go-sqs/queue/option"
 )
 
 type Queue struct {
@@ -36,6 +39,58 @@ func (q *Queue) SendMessage(body string, opts ...option.SendMessageRequest) erro
 
 	_, err := q.SQS.SendMessage(req)
 	return err
+}
+
+type BatchMessage struct {
+	Body    string
+	Options []option.SendMessageRequest
+}
+
+type BatchError struct {
+	Entry sqs.BatchResultErrorEntry
+}
+
+func (e *BatchError) Error() string {
+	return fmt.Sprintf("sqs: id: %s, code: %s, is_sender_failt: %s: %s",
+		e.Entry.ID,
+		e.Entry.Code,
+		e.Entry.SenderFault,
+		e.Entry.Message,
+	)
+}
+
+func (q *Queue) SendMessageBatch(messages ...BatchMessage) error {
+	entries := make([]sqs.SendMessageBatchRequestEntry, len(messages))
+	for i, bm := range messages {
+		req_ := &sqs.SendMessageRequest{}
+		for _, f := range bm.Options {
+			f(req_)
+		}
+
+		entries[i] = sqs.SendMessageBatchRequestEntry{
+			DelaySeconds:      req_.DelaySeconds,
+			MessageAttributes: req_.MessageAttributes,
+			MessageBody:       aws.String(bm.Body),
+			ID:                aws.String(fmt.Sprintf("msg-%d", i)),
+		}
+	}
+
+	req := &sqs.SendMessageBatchRequest{
+		Entries:  entries,
+		QueueURL: q.URL,
+	}
+
+	resp, err := q.SQS.SendMessageBatch(req)
+	if err != nil {
+		return err
+	}
+
+	var result error
+	for _, entry := range resp.Failed {
+		result = multierror.Append(result, &BatchError{Entry: entry})
+	}
+
+	return result
 }
 
 func (q *Queue) ReceiveMessage(opts ...option.ReceiveMessageRequest) ([]sqs.Message, error) {
