863f652bf455ebe0b032c280103b7a4b8ede41d9
TANABE Ken-ichi <nabeken@tknetworks.org>
2023-09-20 00:27:44 +0900

    chore(test-multiqueue): update the test scenario

--- example/test-multiqueue/t.sh
+++ example/test-multiqueue/t.sh
@@ -7,19 +7,19 @@ run_mq_test() {
   curl -sSL "http://127.0.0.1:9003/?index=0&duration=3m&error_rate=0.8" | jq -r .
   sleep 180
 
-  # wait for 60 seconds to converge
+  # wait for 60 seconds for the circuit to be half-open
   sleep 60
 
   echo "$(date) 80% error rate for queue 2 for 3m"
   curl -sSL "http://127.0.0.1:9003/?index=1&duration=3m&error_rate=0.8" | jq -r .
   sleep 180
 
-  # wait for 30 seconds
-  sleep 30
+  # wait for 60 seconds for the circuit to be half-open
+  sleep 60
 
-  echo "$(date) 80% error rate for queue 1 and 2 for 3m and 4m"
-  curl -sSL "http://127.0.0.1:9003/?index=0&duration=3m&error_rate=0.8" | jq -r .
-  curl -sSL "http://127.0.0.1:9003/?index=1&duration=4m&error_rate=0.8" | jq -r .
+  echo "$(date) 80% error rate for queue 1 and 2 for 5m and 7m"
+  curl -sSL "http://127.0.0.1:9003/?index=0&duration=5m&error_rate=0.8" | jq -r .
+  curl -sSL "http://127.0.0.1:9003/?index=1&duration=7m&error_rate=0.8" | jq -r .
   sleep 120
 }
 
